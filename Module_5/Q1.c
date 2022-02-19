051f074a7730488bae1a680ca35be9142b63b462
Sreemannarayana15 <sreemannarayanac2003@gmail.com>
2022-02-19 23:16:45 +0530

    Added Structure Programs

--- Module_5/Q1.c
+++ Module_5/Q1.c
@@ -11,8 +11,8 @@ int main(void)
     struct student s1;
     printf("Enter name : ");
     scanf("%s", s1.name);
-    printf("Enter Roll Number", &s1.number);
-    printf("Enter marks : ", &s1.marks);
-    printf("Details of student : \nName : %s \nRoll Number : %d \nMarks : %.2f", s1.name, s1.number, s1.marks);
+    printf("Enter Roll Number : "); scanf("%d", &s1.number);
+    printf("Enter marks : "); scanf("%f", &s1.marks);
+    printf("\nDetails of student : \nName : %s \nRoll Number : %d \nMarks : %.2f", s1.name, s1.number, s1.marks);
     return 0;
 }
