65c91d02755a260b6b22a512d41f94c495441050
Chamarti Sreemannarayana <93900514+Sreemannarayana15@users.noreply.github.com>
2022-02-21 23:56:54 +0530

    Fixed a comment

--- Module_5/fileReading.c
+++ Module_5/fileReading.c
@@ -3,7 +3,7 @@
 int main()
 {
     FILE *f;    // file pointer
-    f = fopen("C:/Users/LENOVO/Desktop/Computer Science/sample.txt", "r");  // opening file for opening
+    f = fopen("C:/Users/LENOVO/Desktop/Computer Science/sample.txt", "r");  // opening file for reading
     char text[1024]; // to store the data we read
     fscanf(f, "%s", text);  // storing the data from the file into text[1024] character array.
     printf("Line is : %s", text);   // printing the data we got.
